ccc8bf32328b40dbb0fa45dbbb24db02169b1e1b
Louiza-Dev <louizaachiche@gmail.com>
2021-06-13 21:06:35 +0200

    Depot d'Exo1part1PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css">
+    <title>Part1 - Exo1-2</title>
+  </head>
+  <body>
+    <h1>Part1 - Exo1-2</h1>
+    <?php
+    $debut = 'Bonjour, moi c\'est ';
+    $prenom = 'Louiza';
+    $age = '35 ans.';
+    echo $debut, $prenom, ' et j\'ai ', $age;
+    echo '<br>';
+    $prenom = 'Rana';
+    $age = 33;
+    echo $debut, $prenom, ' et j\'ai ', $age, ' ans.';
+    ?>
+  </body>
+</html>
